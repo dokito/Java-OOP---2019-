ddafc9998db85a060724cce744d26dd69665ad3c
Krasimir Kostadinov <kr.m.kostadinov@gmail.com>
2019-07-08 21:45:18 +0300

    Add files via upload

---
+++ NeedForSpeed/Main.java
@@ -0,0 +1,12 @@
+package NeedForSpeed;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//        RaceMotorcycle raceMotorcycle = new RaceMotorcycle(10, 100);
+//
+//        raceMotorcycle.drive(100);
+//        System.out.println(raceMotorcycle.getFuel());
+    }
+}
